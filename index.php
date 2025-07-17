fd79df287f59025e2570eb622102c6e7b2ee45c9
Michael Leano <leanosanchezmichaelandres@gmail.com>
2025-07-17 16:02:46 -0500

    Inicial commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+// Tipos de datos
+// numeros
+$edad = 42;
+$num_decimal = 12.2;
+
+// Texto
+$numero_texto = "2";
+
+// Boleanos
+$mayor_edad = false;
+
+/* if ($mayor_edad === true) 
+{
+    echo  "Eres mayor de edad";
+}
+else 
+{
+    echo "No eres mayor de edad";
+} */
+
+$edad = "23";
+
+// Condicionales
+/* if ($edad == 23)
+{
+    echo "Tienes 23 años";
+}
+else if ($edad == 17)
+{
+    echo "Hola profe";
+}
+else 
+{
+    echo "No tienes 23 años";
+} */
+
+
+// Loops
+// For loop es cuando sabemos cuantas beses queremos repetir una accion
+$contador = 5;
+
+/* for ($i = 0; $i < $contador; $i++)
+{
+    echo $i + 1;
+}
+ */
+// FOREACH Acceder a los items de una lista/array
+/* $boleanos = [true, false, true, true, false];
+
+foreach ($boleanos as $boleano) {
+    if ($boleano == true)
+    {
+        echo "valor es verdadero <br>";
+    }
+    else {
+        echo "valor falso <br>";
+    }
+} */
+
+
+// Cuando no sabemos cuantas veces debemos repetir una accion
+/* $i = 20;
+while ($i > 10) {
+    $i--;
+
+    echo "i es igual a " . $i . "<br>";
+} */
+
+    
